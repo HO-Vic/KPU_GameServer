7c41754aee40ce16cd97762fd661c669ee56ab3e
ParkSeungHo <2018184010@tukorea.ac.kr>
2022-11-25 05:35:20 +0900

    DB함수 주석

---
+++ 과제4/Mulit_IOCP VIEW, DB/MultiIOCP DB/MultiIOCP/GameServerMulti/main.cpp
@@ -0,0 +1,472 @@
+#include <iostream>
+#include <array>
+#include <WS2tcpip.h>
+#include <MSWSock.h>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include<random>
+#include<chrono>
+#include <unordered_set>
+#include<sqlext.h>
+#include "protocol.h"
+
+#pragma comment(lib, "WS2_32.lib")
+#pragma comment(lib, "MSWSock.lib")
+using namespace std;
+using namespace chrono;
+constexpr int MAX_USER = 10000;
+
+random_device rd;
+default_random_engine dre(rd());
+uniform_int_distribution<int> uid(0, 400);
+
+
+enum COMP_TYPE { OP_ACCEPT, OP_RECV, OP_SEND };
+class OVER_EXP {
+public:
+	WSAOVERLAPPED _over;
+	WSABUF _wsabuf;
+	char _send_buf[BUF_SIZE];
+	COMP_TYPE _comp_type;
+	OVER_EXP()
+	{
+		_wsabuf.len = BUF_SIZE;
+		_wsabuf.buf = _send_buf;
+		_comp_type = OP_RECV;
+		ZeroMemory(&_over, sizeof(_over));
+	}
+	OVER_EXP(char* packet)
+	{
+		_wsabuf.len = packet[0];
+		_wsabuf.buf = _send_buf;
+		ZeroMemory(&_over, sizeof(_over));
+		_comp_type = OP_SEND;
+		memcpy(_send_buf, packet, packet[0]);
+	}
+};
+
+enum S_STATE { ST_FREE, ST_ALLOC, ST_INGAME };
+class SESSION {
+	OVER_EXP _recv_over;
+
+public:
+	mutex _s_lock;
+	S_STATE _state;
+	int _id;
+	SOCKET _socket;
+	short	x, y;
+	char	_name[NAME_SIZE];
+	int		_prev_remain;
+	unordered_set <int> _view_list;//이 클라의 뷰 리스트
+	mutex	_vl; // 뷰 리스트 전용 락
+public:
+	SESSION()
+	{
+		_id = -1;
+		_socket = 0;
+		x = y = 0;
+		_name[0] = 0;
+		_state = ST_FREE;
+		_prev_remain = 0;
+	}
+
+	~SESSION() {}
+
+	void do_recv()
+	{
+		DWORD recv_flag = 0;
+		memset(&_recv_over._over, 0, sizeof(_recv_over._over));
+		_recv_over._wsabuf.len = BUF_SIZE - _prev_remain;
+		_recv_over._wsabuf.buf = _recv_over._send_buf + _prev_remain;
+		WSARecv(_socket, &_recv_over._wsabuf, 1, 0, &recv_flag,
+			&_recv_over._over, 0);
+	}
+
+	void do_send(void* packet)
+	{
+		OVER_EXP* sdata = new OVER_EXP{ reinterpret_cast<char*>(packet) };
+		WSASend(_socket, &sdata->_wsabuf, 1, 0, 0, &sdata->_over, 0);
+	}
+	void send_login_info_packet()
+	{
+		SC_LOGIN_INFO_PACKET p;
+		p.id = _id;
+		p.size = sizeof(SC_LOGIN_INFO_PACKET);
+		p.type = SC_LOGIN_INFO;
+		x = p.x = uid(dre);
+		y = p.y = uid(dre);
+		do_send(&p);
+	}
+	void send_move_packet(int c_id);
+	void send_add_player_packet(int c_id);
+	void send_remove_player_packet(int c_id)
+	{
+		_vl.lock();	 
+		if (_view_list.count(c_id))
+			_view_list.erase(c_id);
+		else {
+			_vl.unlock();
+			return;
+		}
+		_vl.unlock();
+		SC_REMOVE_PLAYER_PACKET p;
+		p.id = c_id;
+		p.size = sizeof(p);
+		p.type = SC_REMOVE_PLAYER;
+		do_send(&p);
+	}
+};
+
+bool InitializeDB();
+
+array<SESSION, MAX_USER> clients;
+SOCKET g_s_socket, g_c_socket;
+OVER_EXP g_a_over;
+
+SQLHENV henv;
+SQLHDBC hdbc;
+
+bool can_see(int from, int to)
+{
+	if (abs(clients[from].x - clients[to].x) > VIEW_RANGE) return false;
+	return abs(clients[from].y - clients[to].y) <= VIEW_RANGE;
+}
+
+void SESSION::send_move_packet(int c_id)
+{
+	SC_MOVE_PLAYER_PACKET p;
+	p.id = c_id;
+	p.size = sizeof(SC_MOVE_PLAYER_PACKET);
+	p.type = SC_MOVE_PLAYER;
+	p.x = clients[c_id].x;
+	p.y = clients[c_id].y;
+	p.move_time = duration_cast<milliseconds>(high_resolution_clock::now().time_since_epoch()).count();
+	do_send(&p);
+}
+
+void SESSION::send_add_player_packet(int c_id)
+{
+	SC_ADD_PLAYER_PACKET add_packet;
+	add_packet.id = c_id;
+	strcpy_s(add_packet.name, clients[c_id]._name);
+	add_packet.size = sizeof(add_packet);
+	add_packet.type = SC_ADD_PLAYER;
+	add_packet.x = clients[c_id].x;
+	add_packet.y = clients[c_id].y;
+	_vl.lock();
+	_view_list.insert(c_id);
+	_vl.unlock();
+	do_send(&add_packet);
+}
+
+int get_new_client_id()
+{
+	for (int i = 0; i < MAX_USER; ++i) {
+		lock_guard <mutex> ll{ clients[i]._s_lock };
+		if (clients[i]._state == ST_FREE)
+			return i;
+	}
+	return -1;
+}
+
+void process_packet(int c_id, char* packet)
+{
+	switch (packet[1]) {
+	case CS_LOGIN: {
+		CS_LOGIN_PACKET* p = reinterpret_cast<CS_LOGIN_PACKET*>(packet);
+		strcpy_s(clients[c_id]._name, p->name);
+		clients[c_id].send_login_info_packet();
+		{
+			lock_guard<mutex> ll{ clients[c_id]._s_lock };
+			clients[c_id]._state = ST_INGAME;
+		}
+		for (auto& pl : clients) {
+			{
+				lock_guard<mutex> ll(pl._s_lock);
+				if (ST_INGAME != pl._state) continue;
+			}
+			if (pl._id == c_id) continue;
+			if (false == can_see(c_id, pl._id))
+				continue;
+			clients[c_id]._vl.lock();
+			clients[c_id]._view_list.insert(pl._id);
+			clients[c_id]._vl.unlock();
+			pl.send_add_player_packet(c_id);
+			clients[c_id].send_add_player_packet(pl._id);
+		}
+		break;
+	}
+	case CS_MOVE: {
+		CS_MOVE_PACKET* p = reinterpret_cast<CS_MOVE_PACKET*>(packet);
+		short x = clients[c_id].x;
+		short y = clients[c_id].y;
+		switch (p->direction) {
+		case 1: if (y > 0) y--; break;
+		case 2: if (y < W_HEIGHT - 1) y++; break;
+		case 3: if (x > 0) x--; break;
+		case 4: if (x < W_WIDTH - 1) x++; break;
+		}
+		clients[c_id].x = x;
+		clients[c_id].y = y;
+
+		unordered_set<int> near_list;
+
+		clients[c_id]._vl.lock();
+		unordered_set<int> old_vlist = clients[c_id]._view_list;
+		clients[c_id]._vl.unlock();
+
+		for (auto& cl : clients) {
+			if (cl._state != ST_INGAME) continue;
+			if (cl._id == c_id) continue;
+			if (can_see(c_id, cl._id))
+				near_list.insert(cl._id);
+		}
+
+		clients[c_id].send_move_packet(c_id);
+
+		for (auto& pl : near_list) {
+			auto& cpl = clients[pl];
+			cpl._vl.lock();
+			if (cpl._view_list.count(c_id) > 0) {
+				cpl._vl.unlock();
+				cpl.send_move_packet(c_id);
+			}
+			else {
+				cpl._vl.unlock();
+				clients[pl].send_add_player_packet(c_id);
+			}
+
+			if (old_vlist.count(pl) == 0)
+				clients[c_id].send_add_player_packet(pl);
+		}
+
+		for (auto& pl : old_vlist)
+			if (0 == near_list.count(pl)) {
+				clients[c_id].send_remove_player_packet(pl);
+
+				clients[pl].send_remove_player_packet(c_id);
+			}
+	}
+				break;
+	}
+}
+
+void disconnect(int c_id)
+{
+	clients[c_id]._vl.lock();
+	unordered_set <int> vl = clients[c_id]._view_list;
+	clients[c_id]._vl.unlock();
+	for (auto& p_id : vl) {
+		auto& pl = clients[p_id];
+		{
+			lock_guard<mutex> ll(pl._s_lock);
+			if (ST_INGAME != pl._state) continue;
+		}
+		if (pl._id == c_id) continue;
+		pl.send_remove_player_packet(c_id);
+	}
+	closesocket(clients[c_id]._socket);
+
+	lock_guard<mutex> ll(clients[c_id]._s_lock);
+	clients[c_id]._state = ST_FREE;
+}
+
+void worker_thread(HANDLE h_iocp)
+{
+	while (true) {
+		DWORD num_bytes;
+		ULONG_PTR key;
+		WSAOVERLAPPED* over = nullptr;
+		BOOL ret = GetQueuedCompletionStatus(h_iocp, &num_bytes, &key, &over, INFINITE);
+		OVER_EXP* ex_over = reinterpret_cast<OVER_EXP*>(over);
+		if (FALSE == ret) {
+			if (ex_over->_comp_type == OP_ACCEPT) cout << "Accept Error";
+			else {
+				cout << "GQCS Error on client[" << key << "]\n";
+				disconnect(static_cast<int>(key));
+				if (ex_over->_comp_type == OP_SEND) delete ex_over;
+				continue;
+			}
+		}
+
+		switch (ex_over->_comp_type) {
+		case OP_ACCEPT: {
+			int client_id = get_new_client_id();
+			if (client_id != -1) {
+				{
+					lock_guard<mutex> ll(clients[client_id]._s_lock);
+					clients[client_id]._state = ST_ALLOC;
+				}
+				clients[client_id].x = 0;
+				clients[client_id].y = 0;
+				clients[client_id]._id = client_id;
+				clients[client_id]._name[0] = 0;
+				clients[client_id]._prev_remain = 0;
+				clients[client_id]._view_list.clear();
+				clients[client_id]._socket = g_c_socket;
+				CreateIoCompletionPort(reinterpret_cast<HANDLE>(g_c_socket),
+					h_iocp, client_id, 0);
+				clients[client_id].do_recv();
+				g_c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+			}
+			else {
+				cout << "Max user exceeded.\n";
+			}
+			ZeroMemory(&g_a_over._over, sizeof(g_a_over._over));
+			int addr_size = sizeof(SOCKADDR_IN);
+			AcceptEx(g_s_socket, g_c_socket, g_a_over._send_buf, 0, addr_size + 16, addr_size + 16, 0, &g_a_over._over);
+			break;
+		}
+		case OP_RECV: {
+			int remain_data = num_bytes + clients[key]._prev_remain;
+			char* p = ex_over->_send_buf;
+			while (remain_data > 0) {
+				int packet_size = p[0];
+				if (packet_size <= remain_data) {
+					process_packet(static_cast<int>(key), p);
+					p = p + packet_size;
+					remain_data = remain_data - packet_size;
+				}
+				else break;
+			}
+			clients[key]._prev_remain = remain_data;
+			if (remain_data > 0) {
+				memcpy(ex_over->_send_buf, p, remain_data);
+			}
+			clients[key].do_recv();
+			break;
+		}
+		case OP_SEND:
+			delete ex_over;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	HANDLE h_iocp;
+
+	WSADATA WSAData;
+	WSAStartup(MAKEWORD(2, 2), &WSAData);
+	g_s_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	SOCKADDR_IN server_addr;
+	memset(&server_addr, 0, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT_NUM);
+	server_addr.sin_addr.S_un.S_addr = INADDR_ANY;
+	bind(g_s_socket, reinterpret_cast<sockaddr*>(&server_addr), sizeof(server_addr));
+	listen(g_s_socket, SOMAXCONN);
+	SOCKADDR_IN cl_addr;
+	int addr_size = sizeof(cl_addr);
+	int client_id = 0;
+
+	h_iocp = CreateIoCompletionPort(INVALID_HANDLE_VALUE, 0, 0, 0);
+	CreateIoCompletionPort(reinterpret_cast<HANDLE>(g_s_socket), h_iocp, 9999, 0);
+	g_c_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
+	g_a_over._comp_type = OP_ACCEPT;
+	AcceptEx(g_s_socket, g_c_socket, g_a_over._send_buf, 0, addr_size + 16, addr_size + 16, 0, &g_a_over._over);
+
+	vector <thread> worker_threads;
+	int num_threads = std::thread::hardware_concurrency();
+	for (int i = 0; i < num_threads; ++i)
+		worker_threads.emplace_back(worker_thread, h_iocp);
+	for (auto& th : worker_threads)
+		th.join();
+	closesocket(g_s_socket);
+	WSACleanup();
+}
+
+bool InitializeDB()
+{
+	SQLHSTMT hstmt = 0; // sql명령을 저장하고 핸들을 이용해서 실행하기 위한 핸들
+	SQLRETURN retcode;
+
+	SQLWCHAR szName[NAME_LEN];
+	SQLINTEGER szLevel, szId;
+	SQLLEN cbName = 0, cbLevel = 0, cbId = 0;
+
+	//https://ozt88.tistory.com/43
+	//설명 괜찮은집 발견 -> 조금 봐도 될듯?
+
+	// Allocate environment handle  
+	retcode = SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &henv);//환경 핸들 할당
+
+	// Set the ODBC version environment attribute  
+	if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+		retcode = SQLSetEnvAttr(henv, SQL_ATTR_ODBC_VERSION, (SQLPOINTER*)SQL_OV_ODBC3, 0);//환경핸들에 환경 속성을 설정
+									  //어떤 attribute인데			//저 attib에 이런 정보를		환경 핸들에 매핑할거다.
+		// Allocate connection handle  
+		if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+			retcode = SQLAllocHandle(SQL_HANDLE_DBC, henv, &hdbc); //1번인자: 어떤 종류의 데이터 유형인지, 2번 이 환경 속성에 대해서 3번 할당된 데이터를 핸들에 주소 연결
+
+			// Set login timeout to 5 seconds  
+			if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+				SQLSetConnectAttr(hdbc, SQL_LOGIN_TIMEOUT, (SQLPOINTER)5, 0); //연결 설정
+
+				// Connect to data source  
+				retcode = SQLConnect(hdbc, (SQLWCHAR*)L"gameServer", SQL_NTS, (SQLWCHAR*)NULL, 0, NULL, 0);
+//											 //서버 이름		서버이름 크기	유저이름	길이  인증 인증문자열 크기
+				// Allocate statement handle  
+				if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+					retcode = SQLAllocHandle(SQL_HANDLE_STMT, hdbc, &hstmt);//stmt(statement?)는 명령문 할당 핸들
+
+					//retcode = SQLExecDirect(hstmt, (SQLWCHAR*)L"SELECT Name, User_ID, [Level] FROM Table_1", SQL_NTS);
+					retcode = SQLExecDirect(hstmt, (SQLWCHAR*)L"EXEC select_user_over 10", SQL_NTS);
+					/*SQLExecDirect 는 문에 매개 변수가 있는 경우 매개 변수 표식 변수의 현재 값을 사용하여 준비 가능한 문을 실행합니다.
+					SQLExecDirect 는 일회성 실행을 위해 SQL 문을 제출하는 가장 빠른 방법입니다. -> 일회성??*/
+
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+
+						// Bind columns 1, 2, and 3  
+						/*retcode = SQLBindCol(hstmt, 1, SQL_C_WCHAR, &szName, NAME_LEN, &cbName);
+						retcode = SQLBindCol(hstmt, 2, SQL_C_LONG, &szId, 4, &cbId);
+						retcode = SQLBindCol(hstmt, 3, SQL_C_LONG, &szLevel, 4, &cbLevel);*/
+
+						retcode = SQLBindCol(hstmt, 1, SQL_C_WCHAR, szName, NAME_LEN, &cbName);// 데이터버퍼와 결과 집합의 열에 바인딩
+						//stmt  몇번째 열, c형식 타입, sql로 선언된 변수, 담을 수 있는 최대 길이?, 실제 담은 길이?
+						retcode = SQLBindCol(hstmt, 2, SQL_C_LONG, &szId, 4, &cbId);
+						retcode = SQLBindCol(hstmt, 3, SQL_C_LONG, &szLevel, 4, &cbLevel);
+
+						// Fetch and print each row of data. On an error, display a message and exit.  
+						for (int i = 0; ; i++) {
+							retcode = SQLFetch(hstmt); // 다음 행일 가져와라 명령어
+							if (retcode == SQL_ERROR || retcode == SQL_SUCCESS_WITH_INFO)
+								//show_error();
+							if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO)
+							{
+								//replace wprintf with printf
+								//%S with %ls
+								//warning C4477: 'wprintf' : format string '%S' requires an argument of type 'char *'
+								//but variadic argument 2 has type 'SQLWCHAR *'
+								//wprintf(L"%d: %S %S %d\n", i + 1, szLevel, szName, szId);
+								printf("%d: %d %ls %d\n", i + 1, szLevel, szName, szId);
+								//std::cout << szName << " " << szId << " " << szLevel << std::endl;
+								//printf("%d: %ls %ls %ls\n", i + 1, sCustID, szName, szPhone);
+							}
+							else
+								break;
+						}
+					}
+					else {
+						if (retcode == SQL_ERROR) {
+							std::cout << "error" << std::endl;
+						}
+					}
+					// Process data  
+					if (retcode == SQL_SUCCESS || retcode == SQL_SUCCESS_WITH_INFO) {
+						SQLCancel(hstmt);///종료
+						SQLFreeHandle(SQL_HANDLE_STMT, hstmt);//리소스 해제
+					}
+
+					SQLDisconnect(hdbc);
+				}
+
+				SQLFreeHandle(SQL_HANDLE_DBC, hdbc);
+			}
+		}
+		SQLFreeHandle(SQL_HANDLE_ENV, henv);
+	}
+	return false;
+}
